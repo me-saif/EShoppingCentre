1327eadbcae22dc4b2d9774f883937b9ec1b6fb8
me.saif <me.shaikhsaif@gmail.com>
2022-03-17 23:42:55 +0530

    Create Areas for Admin and Customer and apply bootswatch sketchy theme

--- EShoppingCentre/EShoppingCentre.Web/Areas/Customer/Controllers/HomeController.cs
+++ EShoppingCentre/EShoppingCentre.Web/Areas/Customer/Controllers/HomeController.cs
@@ -1,14 +1,11 @@
 ﻿using EShoppingCentre.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EShoppingCentre.Web.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;

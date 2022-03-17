e5a9a9dbaecb90ad69b9edc73f05a665f6be3154
me.saif <me.shaikhsaif@gmail.com>
2022-03-17 20:10:42 +0530

    Build connection and create identitydbcontext with product and category

---
+++ EShoppingCentre/EShoppingCentre.DataAccess/EShoppingCentreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EShoppingCentre.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShoppingCentre.DataAccess
+{
+    public class EShoppingCentreDbContext : IdentityDbContext
+    {
+        public EShoppingCentreDbContext(DbContextOptions<EShoppingCentreDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
